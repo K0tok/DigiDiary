d6ff7c339d2f2e8fc0b5d92e9c4ce9ed2bff8971
Konstantin <bodrov.2005@inbox.ru>
2025-04-29 13:41:58 +0500

    Добавил кнопки с коммандами, разделил расписание на неделю/сегодня

--- keyboards.py
+++ keyboards.py
@@ -3,7 +3,8 @@ from functions import get_groups
 from DataBase import select_groups
 
 keyboard_commands = telebot.types.ReplyKeyboardMarkup(True)
-keyboard_commands.row("/schedule")
+keyboard_commands.row("🗓️ Расписание занятий НТИ", "📅 Расписание на сегодня")
+keyboard_commands.row("👤 Мой профиль", "📖 Дневник")
 
 
 simpleAnswerKeyboard = telebot.types.InlineKeyboardMarkup()
@@ -11,7 +12,7 @@ yesButton = telebot.types.InlineKeyboardButton("Да", callback_data="yes")
 noButton = telebot.types.InlineKeyboardButton("Нет", callback_data="no")
 simpleAnswerKeyboard.row(yesButton, noButton)
 
-keyboard_groups = telebot.types.InlineKeyboardMarkup(row_width=2)
+keyboard_groups_week = telebot.types.InlineKeyboardMarkup(row_width=2)
 groups = select_groups()
 buttons = []
 for group in groups:
@@ -25,8 +26,28 @@ for group in groups:
     
     button = telebot.types.InlineKeyboardButton(
         text=button_text,
-        callback_data=f"group_{group['name']}"
+        callback_data=f"group_{group['name']}_week"
     )
     buttons.append(button)
 
-keyboard_groups.add(*buttons)
+keyboard_groups_week.add(*buttons)
+
+keyboard_groups_today = telebot.types.InlineKeyboardMarkup(row_width=2)
+groups = select_groups()
+buttons = []
+for group in groups:
+    group_name = group['name']
+    
+    # Проверяем, принадлежит ли группа пользователю
+    if group_name in ['Т-143901-ИСТ', 'Т-233901-ИСТ']:              # ДОБАВИТЬ ПРОСМОТР ГРУПП ПОЛЬЗОВАТЕЛЯ
+        button_text = f"<< {group_name} >>" 
+    else:
+        button_text = group_name
+    
+    button = telebot.types.InlineKeyboardButton(
+        text=button_text,
+        callback_data=f"group_{group['name']}_today"
+    )
+    buttons.append(button)
+
+keyboard_groups_today.add(*buttons)
--- main.py
+++ main.py
@@ -15,15 +15,14 @@ url = config.URL
 def send_welcome(message): 
     user_id = message.from_user.id
     if user_id not in select_users_tgId():
-        bot.send_message(message.chat.id, "Добро пожаловать в ЭДС!")
+        bot.send_message(message.chat.id, "Добро пожаловать в ЭДС!", reply_markup=keyboard_commands)
         # bot.send_message(message.chat.id, f"{message.chat.id}" , reply_markup=keyboard1)
         user_first_name = message.from_user.first_name if message.from_user.first_name != None else ""
         user_last_name = message.from_user.last_name if message.from_user.last_name != None else ""
         user_name = user_first_name + user_last_name if user_first_name + user_last_name != "" else message.from_user.username
         add_user(user_id, user_name)
     else:
-        bot.send_message(message.chat.id, f"Рады видеть вас снова, {message.from_user.first_name}!")
-        # bot.send_message(message.chat.id, f"{message.chat.id}" , reply_markup=keyboard1)
+        bot.send_message(message.chat.id, f"Рады видеть вас снова, {message.from_user.first_name}!", reply_markup=keyboard_commands)
 
 
 @bot.message_handler(commands=["schedule"])
@@ -31,7 +30,7 @@ def send_schedule_simple(message):
         # Разбираем текст сообщения
         command, *args = message.text.split()
         if len(args) < 1:
-            bot.reply_to(message, "Пожалуйста, выберите группу или введите номер в комманду. \n<i>Пример: /schedule Т-143901-ИСТ</i>", reply_markup=keyboard_groups)
+            bot.reply_to(message, "Пожалуйста, выберите группу или введите номер в комманду. \n<i>Пример: /schedule Т-143901-ИСТ</i>", reply_markup=keyboard_groups_week)
             return
         else: 
             group_name = args[0].upper()  # Название группы
@@ -50,8 +49,9 @@ def send_schedule_simple(message):
 @bot.callback_query_handler(func=lambda call: call.data.startswith("group_"))
 def send_schedule(call):
     group_name = call.data.split("_")[1]
+    today_only = True if call.data.split("_")[2] == "today" else False
 
-    schedule_text = get_schedule(url, group_name)
+    schedule_text = get_schedule(url, group_name, today_only)
 
     bot.send_message(call.message.chat.id, schedule_text)
 
@@ -73,8 +73,12 @@ def send_about(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message: telebot.types.Message):
-    # if message.text == "Расписание на сегодня":
-
+    if message.text in ["🗓️ Расписание занятий НТИ", "Расписание занятий НТИ", "🗓️"]:
+        bot.reply_to(message, "Пожалуйста, выберите группу или введите номер в комманду. \n<i>Пример: /schedule Т-143901-ИСТ</i>", reply_markup=keyboard_groups_week)
+        return
+    if message.text in ["📅 Расписание на сегодня", "Расписание на сегодня", "📅"]:
+        bot.reply_to(message, "Пожалуйста, выберите группу или введите номер в комманду. \n<i>Пример: /schedule Т-143901-ИСТ</i>", reply_markup=keyboard_groups_today)
+        return
     if message.text == "Привет":
         bot.reply_to(message, f"Привет, {message.from_user.first_name}!")
     if message.text == "Пока":
